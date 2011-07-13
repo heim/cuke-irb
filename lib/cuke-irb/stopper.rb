11aeb032cb3627fb8ed1dfbda9e8e5efdd17190f
Andreas Heim <andreas@heim.no>
2011-07-13 09:52:53 +0200

    initial commit

---
+++ lib/cuke-irb/stopper.rb
@@ -0,0 +1,13 @@
+module CukeIrb
+  if respond_to? :After
+    After do |scenario|
+      if scenario.failed? &&  scenario.source_tag_names.include?("@stop")
+        puts "Scenario failed. You are in rails console becuase of @stop. Type exit when you are done"
+        require 'irb'
+        require 'irb/completion'
+        ARGV.clear
+        IRB.start
+      end  
+    end
+  end
+end
---
+++ lib/slowhandcuke.rb
@@ -0,0 +1,1 @@
+require 'cuke-irb/stopper'
