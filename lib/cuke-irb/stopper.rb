66ab513929380d79a87080db523b20f5d65642db
Andreas Heim <andreas@heim.no>
2011-07-13 10:13:01 +0200

    reverted change

--- lib/cuke-irb/stopper.rb
+++ lib/cuke-irb/stopper.rb
@@ -2,7 +2,7 @@ require 'cucumber'
 if respond_to? :After
   After do |scenario|
     if scenario.failed? && scenario.source_tag_names.include?("@stop") && scenario.source_tag_names.include?("@stop")
-      @io.printf "Scenario failed. You are in irb because of @stop. Type exit when you are done"
+      puts "Scenario failed. You are in irb because of @stop. Type exit when you are done"
       require 'irb'
       require 'irb/completion'
       ARGV.clear
