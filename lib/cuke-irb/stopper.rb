0d826b681fd56cd94783db3779b3a7fbe504d73e
Andreas Heim <andreas@heim.no>
2011-07-13 10:08:40 +0200

    better output

--- lib/cuke-irb/stopper.rb
+++ lib/cuke-irb/stopper.rb
@@ -2,10 +2,7 @@ require 'cucumber'
 if respond_to? :After
   After do |scenario|
     if scenario.failed? && scenario.source_tag_names.include?("@stop") && scenario.source_tag_names.include?("@stop")
-      if respond_to? :logger
-        logger.info "Scenario failed. You are in rails console becuase of @stop. Type exit when you are done"
-      end
-      puts "Scenario failed. You are in rails console becuase of @stop. Type exit when you are done"
+      @io.printf "Scenario failed. You are in irb because of @stop. Type exit when you are done"
       require 'irb'
       require 'irb/completion'
       ARGV.clear
